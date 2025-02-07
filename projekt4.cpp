81b321ec1aed99f641aaf48ee41b097cbd06eeb2
eshrefkrapii <eshref.krapi@student.uni-pr.edu>
2025-02-08 00:51:36 +0100

    Shtimi i funksionit menuKryesore

--- projekt4.cpp
+++ projekt4.cpp
@@ -5,11 +5,30 @@
 using namespace std;
 
 
+void menuKryesore();
 
 int main() {
 
+    menuKryesore();
     return 0;
 }
+
+void menuKryesore() {
+    int zgjedhja;
+    do {
+        cout << "\nVegla per Menaxhimin e Notave te Studenteve(VMNS)\n";
+        cout << "3. Dil\n";
+        cout << "Zgjedhni: ";
+        cin >> zgjedhja;
+
+        switch (zgjedhja) {
+
+        case 3: cout << "Mirupafshim!\n"; break;
+        default: cout << "Zgjedhja nuk u gjend ne sherbimet tona. Provoni perseri.\n";
+        }
+    } while (zgjedhja != 3);
+}
+
 
 
 void Kalkulatori() {
