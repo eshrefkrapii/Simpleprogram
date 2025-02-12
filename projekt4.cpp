5c3afdfeefce3711436be502603025d4b56eea90
eshrefkrapii <eshref.krapi@student.uni-pr.edu>
2025-02-12 02:16:26 +0100

    Shtimi i konstanteve, struktures Studenti, variablat globale dhe adresim i Warnings

--- projekt4.cpp
+++ projekt4.cpp
@@ -4,30 +4,54 @@
 
 using namespace std;
 
+// Konstantet per numrin maksimal te studenteve dhe numrin maksimal te notave
+const int STUDENTET_MAX = 100;
+const int NOTA_MAX = 10;
+
+
+// Struktura per marrjen dhe ruajtjen e te dhenave te studenteve
+struct Studenti {
+    string emri;
+    int notat[NOTA_MAX] = {};  // Inicializon fillimisht te gjithe anetaret me 0 
+    int numriNotave = 0;
+
+};
+
+// Variabla globale
+Studenti notateStudenteve[STUDENTET_MAX];
+int numriStudenteve = 0;
 
 void menuKryesore();
+void menaxhoNotat();
+void Kalkulatori();
 
 int main() {
 
     menuKryesore();
+
     return 0;
 }
 
 void menuKryesore() {
+
     int zgjedhja;
     do {
         cout << "\nVegla per Menaxhimin e Notave te Studenteve(VMNS)\n";
+        cout << "1. Menaxho studentet dhe notat\n";
+        cout << "2. Perdor kalkulatorin\n";
         cout << "3. Dil\n";
         cout << "Zgjedhni: ";
         cin >> zgjedhja;
 
         switch (zgjedhja) {
-
+        case 1: menaxhoNotat(); break;
+        case 2: Kalkulatori(); break;
         case 3: cout << "Mirupafshim!\n"; break;
         default: cout << "Zgjedhja nuk u gjend ne sherbimet tona. Provoni perseri.\n";
         }
     } while (zgjedhja != 3);
 }
+
 
 
 void menaxhoNotat() {
@@ -44,97 +68,101 @@ void menaxhoNotat() {
         cout << endl;
 
         switch (zgjedhja) {
-            case 1: {
-                if (numriStudenteve >= STUDENTET_MAX) {
-                    cout << "Nuk mund te shtoni me studente. Limiti eshte arritur.\n";
-                    break;
-                }
-                string emri;
-                int nota, n;
-                cout << "Shkruani emrin e studentit: ";
-                cin >> emri;
-                cout << "Shkruani numrin e notave: ";
-                cin >> n;
-                if (n > NOTA_MAX) {
-                    cout << "Nuk mund te shtoni me shume se " << NOTA_MAX << " nota.\n";
-                    break;
-                }
-                Studenti& studentiRi = notateStudenteve[numriStudenteve++];
-                studentiRi.emri = emri;
-                studentiRi.numriNotave = n;
-                cout << "Shkruani notat: ";
-                for (int i = 0; i < n; ++i) {
-                    cin >> nota;
-                    studentiRi.notat[i] = nota;
-                }
-                cout << "Studenti u shtua me sukses!\n";
+        case 1: {
+            if (numriStudenteve >= STUDENTET_MAX) {
+                cout << "Nuk mund te shtoni me studente. Limiti eshte arritur.\n";
                 break;
             }
-            case 2: {
-                if (numriStudenteve == 0) {
-                    cout << "Nuk ka studente te regjistruar.\n";
-                }
-                else {
-                    for (int i = 0; i < numriStudenteve; ++i) {
-                        cout << notateStudenteve[i].emri << ": ";
-                        for (int j = 0; j < notateStudenteve[i].numriNotave; ++j) {
-                            cout << notateStudenteve[i].notat[j] << " ";
-                        }
-                        cout << "\n";
-                    }
-                }
+            string emri;
+            int nota, n;
+            cout << "Shkruani emrin e studentit: ";
+            cin >> emri;
+            cout << "Shkruani numrin e notave: ";
+            cin >> n;
+            if (n > NOTA_MAX) {
+                cout << "Nuk mund te shtoni me shume se " << NOTA_MAX << " nota.\n";
                 break;
             }
-            case 3: {
-                string emri;
-                cout << "Shkruani emrin e studentit: ";
-                cin >> emri;
-                bool igjetur = false;
+            Studenti& studentiRi = notateStudenteve[numriStudenteve++];
+            studentiRi.emri = emri;
+            studentiRi.numriNotave = n;
+            cout << "Shkruani notat: ";
+            for (int i = 0; i < n; ++i) {
+                cin >> nota;
+                studentiRi.notat[i] = nota;
+            }
+            cout << "Studenti u shtua me sukses!\n";
+            break;
+        }
+        case 2: {
+            if (numriStudenteve == 0) {
+                cout << "Nuk ka studente te regjistruar.\n";
+            }
+            else {
                 for (int i = 0; i < numriStudenteve; ++i) {
-                    if (notateStudenteve[i].emri == emri) {
-                        igjetur = true;
-                        int shuma = 0;
-                        for (int j = 0; j < notateStudenteve[i].numriNotave; ++j) {
-                            shuma += notateStudenteve[i].notat[j];
-                        }
-                        double mesatarja = double(shuma) / notateStudenteve[i].numriNotave;
-                        cout << "Mesatarja e notave te " << emri << " eshte: " << mesatarja << "\n";
-                        break;
-                    }
-                }
-                if (!igjetur) {
-                    cout << "Studenti nuk u gjet.\n";
-                }
-                break;
-            }
-            case 4: {
-                if (numriStudenteve == 0) {
-                    cout << "Nuk ka studente per te vleresuar.\n";
-                    break;
-                }
-                int meELarta = notateStudenteve[0].notat[0];
-                int meEUlta = notateStudenteve[0].notat[0];
-
-                for (int i = 0; i < numriStudenteve; ++i) {
+                    cout << notateStudenteve[i].emri << ": ";
                     for (int j = 0; j < notateStudenteve[i].numriNotave; ++j) {
-                        meELarta = max(meELarta, notateStudenteve[i].notat[j]);
-                        meEUlta = min(meEUlta, notateStudenteve[i].notat[j]);
+                        cout << notateStudenteve[i].notat[j] << " ";
                     }
+                    cout << "\n";
                 }
-
-                cout << "Nota me e larte: " << meELarta << "\n";
-                cout << "Nota me e ulet: " << meEUlta << "\n";
+            }
+            break;
+        }
+        case 3: {
+            string emri;
+            cout << "Shkruani emrin e studentit: ";
+            cin >> emri;
+            bool igjetur = false;
+            for (int i = 0; i < numriStudenteve; ++i) {
+                if (notateStudenteve[i].emri == emri) {
+                    igjetur = true;
+                    int shuma = 0;
+                    for (int j = 0; j < notateStudenteve[i].numriNotave; ++j) {
+                        shuma += notateStudenteve[i].notat[j];
+                    }
+                    double mesatarja = double(shuma) / notateStudenteve[i].numriNotave;
+                    cout << "Mesatarja e notave te " << emri << " eshte: " << mesatarja << "\n";
+                    break;
+                }
+            }
+            if (!igjetur) {
+                cout << "Studenti nuk u gjet.\n";
+            }
+            break;
+        }
+        case 4: {
+            if (numriStudenteve == 0) {
+                cout << "Nuk ka studente per te vleresuar.\n";
                 break;
             }
+            int meELarta = notateStudenteve[0].notat[0];
+            int meEUlta = notateStudenteve[0].notat[0];
 
-            case 5:
-                cout << "Kthyer tek Menuja kryesore...\n";
-                break;
+            for (int i = 0; i < numriStudenteve; ++i) {
+                for (int j = 0; j < notateStudenteve[i].numriNotave; ++j) {
+                    meELarta = max(meELarta, notateStudenteve[i].notat[j]);
+                    meEUlta = min(meEUlta, notateStudenteve[i].notat[j]);
+                }
+            }
 
-            default:
-                cout << "Zgjedhje jo valide. Provoni perseri.\n";
+            cout << "Nota me e larte: " << meELarta << "\n";
+            cout << "Nota me e ulet: " << meEUlta << "\n";
+            break;
+        }
+
+        case 5:
+            cout << "Kthyer tek Menuja kryesore...\n";
+            break;
+
+        default:
+            cout << "Zgjedhje jo valide. Provoni perseri.\n";
 
         }
+    }
+}
+
+
 void Kalkulatori() {
 
     char perserit;
